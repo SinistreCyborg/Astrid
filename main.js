2fa07e2700f8d9c175e6bd9242f650fb0a21ce19
SinistreCyborg <42881111+SinistreCyborg@users.noreply.github.com>
2019-07-16 19:57:00 -0400

    Export Util, Constants, and node-fetch

--- main.js
+++ main.js
@@ -4,5 +4,9 @@ import Astrid from './structures/Astrid';
 export { default as Command } from './structures/Command';
 export { default as Event } from './structures/Event';
 export { default as Console } from './structures/Console';
+export { default as Util } from './structures/Util';
+export * from './structures/Constants';
+
+export { default as fetch } from 'node-fetch';
 
 new Astrid().connect();
