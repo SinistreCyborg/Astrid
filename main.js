506dc52d2f899e029318bab4bcf9958740eecaf6
SinistreCyborg <42881111+SinistreCyborg@users.noreply.github.com>
2019-07-16 16:42:03 -0400

    Disable ESLint for entire file

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
+/* eslint-disable */
 import Astrid from './structures/Astrid';
 
-/* eslint-disable import/prefer-default-export */
 export { default as Command } from './structures/Command';
 export { default as Event } from './structures/Event';
 export { default as Console } from './structures/Console';
