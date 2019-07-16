870600f0f6545594bda90125b30df183f5d0c472
SinistreCyborg <42881111+SinistreCyborg@users.noreply.github.com>
2019-07-16 15:53:21 -0400

    Load commands

--- structures/Astrid.js
+++ structures/Astrid.js
@@ -1,9 +1,27 @@
 import Eris from 'eris';
+import { readdirSync } from 'fs';
+import { Console } from '..';
 
 export default class extends Eris.Client {
 
     constructor() {
         super(process.env.token);
+
+        /**
+         * @type {Map<string, Command>}
+         */
+        this.commands = new Map();
+        readdirSync('./commands/').forEach(async (file) => {
+            if (!file.endsWith('.js')) return;
+
+            const { default: Command } = await import(`../commands/${file}`);
+            const cmd = new Command(this);
+
+            Console.log('CommandLoader', `Loading ${cmd.name} command.`);
+            this.commands.set(cmd.name, cmd);
+
+        });
+
     }
 
 }
