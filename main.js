7edc61e88eeafcfcc04c196e0f51ca1d6d918052
SinistreCyborg <42881111+SinistreCyborg@users.noreply.github.com>
2019-07-16 15:53:17 -0400

    Export Console class

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ import Astrid from './structures/Astrid';
 /* eslint-disable import/prefer-default-export */
 export { default as Command } from './structures/Command';
 export { default as Event } from './structures/Event';
+export { default as Console } from './structures/Console';
 
 new Astrid().connect();
