6de5e5889ac759418adff629f9cde06bd16a1650
SinistreCyborg <42881111+SinistreCyborg@users.noreply.github.com>
2019-07-17 16:40:20 -0400

    isThenable() to see if function is... thenable...

--- structures/Util.js
+++ structures/Util.js
@@ -1,7 +1,20 @@
 export default class {
 
+    isFunction(input) {
+        return typeof input === 'function';
+    }
+
     static codeBlock(lang, code) {
         return `\`\`\`${lang}\n${code}\`\`\``;
+    }
+
+    static isThenable(input) {
+        if (!input) return false;
+        return (input instanceof Promise) || (
+            input !== Promise.prototype
+            && this.isFunction(input.then)
+            && this.isFunction(input.catch)
+        );
     }
 
 }
